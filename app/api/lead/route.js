47f78b0985deeb82d5e34e27c0bcfbec0afca95d
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-03-25 18:05:38 +0100

    Update route.js

--- app/api/lead/route.js
+++ app/api/lead/route.js
@@ -1,7 +1,28 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(req) {
-  const body = await req.json();
+  try {
+    const body = await req.json();
 
-  console.log("LEAD:", body);
+    console.log("LEAD:", body);
 
-  return Response.json({ success: true });
+    await resend.emails.send({
+      from: "onboarding@resend.dev", // temporaire
+      to: "julien.lbc19@gmail.com", //  ton email
+      subject: "Nouveau lead immobilier - Julien",
+      html: `
+        <h2>Nouveau lead</h2>
+        <p>Voici les informations :</p>
+        <pre>${JSON.stringify(body, null, 2)}</pre>
+      `,
+    });
+
+    return Response.json({ success: true });
+
+  } catch (error) {
+    console.error("ERROR:", error);
+    return Response.json({ success: false });
+  }
 }
