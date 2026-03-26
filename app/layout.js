78986b3afe08278fab7dc0029c84d0255285d121
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-03-26 18:00:55 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
 
 export const metadata = {
   title: "Estimation Immobilière",
-  description: "Estimez votre bien en quelques minutes"
+  description: "Estimez votre bien en quelques minutes",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>
+      <body className={poppins.className}>
         <Navbar />
         {children}
       </body>
