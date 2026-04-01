d30dd07c13f87c3412ff44412f56b10005c265d5
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-04-01 18:21:21 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        {/* Logo à gauche */}
+        <div className="footer-left">
+          <Link href="/">
+            <Image
+              src="/logo.jpeg"
+              alt="Sellmyhome"
+              width={120}
+              height={60}
+              priority
+            />
+          </Link>
+        </div>
+
+        {/* Liens + email au centre */}
+        <div className="footer-center">
+          <div className="footer-links">
+            <Link href="/">Accueil</Link>
+            <Link href="/estimation">Estimation</Link>
+            <Link href="/contact">Recherche</Link>
+            <Link href="/presentation">A propos</Link>
+          </div>
+          <p className="footer-email">contact@sellmyhome.fr</p>
+        </div>
+
+        {/* Logo Leggett à droite */}
+        <div className="footer-right">
+          <Image
+            src="/logo_leggett.png"
+            alt="Leggett Immobilier"
+            width={120}
+            height={60}
+            priority
+          />
+        </div>
+
+      </div>
+    </footer>
+  );
+}
