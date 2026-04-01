4de1137912b73eef2a7b18bc0bee6782f7a34f5d
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-04-01 15:32:49 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
         src="/logo.jpeg"
         alt="Sellmyhome"
         width={150}
-        height={100}
+        height={85}
         priority
       />
     </Link>
