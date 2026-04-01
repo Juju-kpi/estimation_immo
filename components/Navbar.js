5302774c38ab6c27ac49e6bf770242e0b618e782
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-04-01 11:29:04 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,6 +9,7 @@ export default function Navbar() {
           <a href="/" className="nav-link">Accueil</a>
           <a href="/estimation" className="nav-cta">Estimation</a>
           <a href="/contact" className="nav-link">Recherche</a>
+          <a href="/presentation" className="nav-link">A propos</a>
         </div>
 
       </div>
