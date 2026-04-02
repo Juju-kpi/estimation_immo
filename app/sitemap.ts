413ed4c62fd6b314b59affc8ee26517cda7afd85
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-04-02 18:38:54 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,28 @@
-// app/sitemap.ts
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   return [
     {
       url: 'https://sellmyhome.fr',
+      lastModified: new Date(),
       priority: 1,
     },
     {
       url: 'https://sellmyhome.fr/estimation',
+      lastModified: new Date(),
       priority: 0.9,
     },
     {
       url: 'https://sellmyhome.fr/contact',
+      lastModified: new Date(),
       priority: 0.8,
     },
     {
       url: 'https://sellmyhome.fr/presentation',
+      lastModified: new Date(),
       priority: 0.8,
     },
     {
       url: 'https://sellmyhome.fr/nous',
+      lastModified: new Date(),
       priority: 0.9,
     },
   ]
