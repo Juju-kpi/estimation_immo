7e6e97d9f8860f1f3ef9a978ce4e74b0000f561c
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-04-08 19:41:00 +0200

    Update HomeClient.js

--- app/HomeClient.js
+++ app/HomeClient.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import { trackClick } from "../components/Tracker";
+import Script from "next/script";
 
 export default function HomeClient() {
   const [showToast, setShowToast] = useState(false);
