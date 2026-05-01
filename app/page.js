4d59c7ed2db407081902e4a9e8462fc60c3956c1
Juju-kpi <122098240+Juju-kpi@users.noreply.github.com>
2026-05-01 12:27:58 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 
 import HomeClient from "./HomeClient";
+export const metadata = {
+  title: "Bienvenue sur SellMyHome | SellMyHome",
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function Home() {
   return <HomeClient />;
